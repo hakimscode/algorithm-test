5093f7e763b547c1f798c035e258d46bf75a1fcb
hakimscode <hhakimsetiawan@gmail.com>
2020-09-03 19:14:02 +0700

    feat: add function pair socks

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-func algorithmNumberOne(socks int, colors []int) int {
+func algorithmNumberOne(colors []int) int {
+	var pairSocks int
 
-	return 0
+	colorMap := make(map[int]int)
+	for i := 0; i < len(colors); i++ {
+		if _, found := colorMap[colors[i]]; found {
+			colorMap[colors[i]]++
+			if colorMap[colors[i]]%2 == 0 {
+				pairSocks++
+			}
+		} else {
+			colorMap[colors[i]] = 1
+		}
+	}
+
+	return pairSocks
 }
 
 func main() {
